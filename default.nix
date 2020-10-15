a55666e1624d7663d09cc157807ab25c0f4badd7
Zach Coyle <zach.coyle@gmail.com>
2020-10-15 19:00:04 -0400

    add mas package

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@
 # commands such as:
 #     nix-build -A mypackage
 
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 {
   # The `lib`, `modules`, and `overlay` names are special
@@ -14,8 +14,6 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
-}
+  mas = pkgs.callPackage ./pkgs/mas { };
 
+}
